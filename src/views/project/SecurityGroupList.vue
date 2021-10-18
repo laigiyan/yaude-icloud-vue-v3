cd5ad22a3efb4ecf75f5d0a08fdba19bd747c12a
ypj <ypj@3dee3642-7118-1743-bb2f-632451eefa8e>
2021-10-18 09:00:00 +0000

    安全组

--- src/views/project/SecurityGroupList.vue
+++ src/views/project/SecurityGroupList.vue
@@ -26,20 +26,20 @@
 
     <!-- 操作按钮区域 -->
     <div class="table-operator">
-<!--      <a-button @click="handleApply" type="primary" icon="plus">申請</a-button>-->
-<!--      <a-button type="primary"  @click="handlePowerOn" >開機</a-button>-->
-<!--      <a-button type="primary"  @click="handleShutDown" >關機</a-button>-->
-<!--      <a-button type="primary"  @click="handleReboot" >重啓</a-button>-->
-<!--      <a-button type="primary"  @click="handleShowConsole" >控制臺</a-button>-->
+      <!--      <a-button @click="handleApply" type="primary" icon="plus">申請</a-button>-->
+      <!--      <a-button type="primary"  @click="handlePowerOn" >開機</a-button>-->
+      <!--      <a-button type="primary"  @click="handleShutDown" >關機</a-button>-->
+      <!--      <a-button type="primary"  @click="handleReboot" >重啓</a-button>-->
+      <!--      <a-button type="primary"  @click="handleShowConsole" >控制臺</a-button>-->
 
 
-<!--      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>-->
-<!--      <a-button type="primary" icon="download" @click="handleExportXls('申請明細檔')">导出</a-button>-->
-<!--      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">-->
-<!--        <a-button type="primary" icon="import">导入</a-button>-->
-<!--      </a-upload>-->
+      <!--      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>-->
+      <!--      <a-button type="primary" icon="download" @click="handleExportXls('申請明細檔')">导出</a-button>-->
+      <!--      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">-->
+      <!--        <a-button type="primary" icon="import">导入</a-button>-->
+      <!--      </a-upload>-->
       <!-- 高级查询区域 -->
-<!--      <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>-->
+      <!--      <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>-->
       <a-dropdown v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay">
           <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
@@ -50,10 +50,10 @@
 
     <!-- table区域-begin -->
     <div>
-      <!--<div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
         <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
         <a style="margin-left: 24px" @click="onClearSelected">清空</a>
-      </div>-->
+      </div>
 
       <a-table
         ref="table"
@@ -65,10 +65,9 @@
         :dataSource="dataSource"
         :pagination="ipagination"
         :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
         class="j-table-force-nowrap"
         @change="handleTableChange">
-
-        <!--:rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"-->
 
         <template slot="htmlSlot" slot-scope="text">
           <div v-html="text"></div>
@@ -102,25 +101,24 @@
             <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
             <a-menu slot="overlay">
 <!--              <a-menu-item>-->
-<!--                <a @click="handleAdjustResource(record)">調整資源</a>-->
-<!--              </a-menu-item>-->
-<!--              <a-menu-item>-->
-<!--                <a @click="handleAddUserProject(record)">指定管理员</a>-->
-<!--              </a-menu-item>-->
+              <!--                <a @click="handleAdjustResource(record)">調整資源</a>-->
+              <!--              </a-menu-item>-->
+              <!--              <a-menu-item>-->
+              <!--                <a @click="handleAddUserProject(record)">指定管理员</a>-->
+              <!--              </a-menu-item>-->
               <a-menu-item>
-               <new-j-select-multi-user v-model="record.projectUserNames" :projectData="record" @ok="handleOK" selectButtonText="管理成員" :query-config="selectUserQueryConfig"/>
               </a-menu-item>
             </a-menu>
-<!--            <a-menu slot="overlay">-->
-<!--              <a-menu-item>-->
-<!--                <a @click="handleDetail(record)">详情</a>-->
-<!--              </a-menu-item>-->
-<!--              <a-menu-item>-->
-<!--                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">-->
-<!--                  <a>删除</a>-->
-<!--                </a-popconfirm>-->
-<!--              </a-menu-item>-->
-<!--            </a-menu>-->
+            <!--            <a-menu slot="overlay">-->
+            <!--              <a-menu-item>-->
+            <!--                <a @click="handleDetail(record)">详情</a>-->
+            <!--              </a-menu-item>-->
+            <!--              <a-menu-item>-->
+            <!--                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">-->
+            <!--                  <a>删除</a>-->
+            <!--                </a-popconfirm>-->
+            <!--              </a-menu-item>-->
+            <!--            </a-menu>-->
 
           </a-dropdown>
         </span>
@@ -129,7 +127,6 @@
     </div>
 
     <os-instance-modal ref="modalForm" @ok="modalFormOk"></os-instance-modal>
-    <Select-User-Project-Modal ref="selectUserProjectModal" @selectFinished="selectOK"></Select-User-Project-Modal>
   </a-card>
 </template>
 
@@ -140,16 +137,12 @@
   import { JeecgListMixin } from '@/mixins/JeecgListMixin'
   import OsInstanceModal from './modules/OsInstanceModal'
   import { deleteAction, getAction,downFile,getFileAccessHttpUrl } from '@/api/manage'
-  import SelectUserProjectModal from './modules/SelectUserProjectModal'
-  import NewJSelectMultiUser from '@/components/jeecgbiz/NewJSelectMultiUser'
 
   export default {
-    name: 'manage',
+    name: 'SecurityGroupList',
     mixins:[JeecgListMixin, mixinDevice],
     components: {
-      OsInstanceModal,
-      SelectUserProjectModal,
-      NewJSelectMultiUser
+      OsInstanceModal
     },
     data () {
       return {
@@ -167,42 +160,19 @@
             }
           },
           {
-            title:'項目名稱',
+            title:'安全组名稱',
             align:"center",
-            dataIndex: 'projectName',
-            scopedSlots: { customRender: 'projectName' }
+            dataIndex: 'name',
+          },
+          {
+            title:'安全组ID',
+            align:"center",
+            dataIndex: 'securityGroupId'
           },
           {
             title:'描述',
             align:"center",
             dataIndex: 'description'
-          },
-          {
-            title:'項目ID',
-            align:"center",
-            dataIndex: 'projectId'
-          },
-          {
-            title:'域名',
-            align:"center",
-            dataIndex: 'domainName'
-          },
-         /*{
-            title:'成员',
-            align:"center",
-            dataIndex: 'projectUserNames'
-          },*/
-          {
-            title:'是否啓用',
-            align:"center",
-            dataIndex: 'enabled',
-            customRender:function (enabled) {
-              if(enabled){
-                return "true";
-              }else {
-                return "false";
-              }
-            }
           },
           {
             title: '操作',
@@ -214,16 +184,8 @@
           }
         ],
         url: {
-          list: "/openstack/osUserProject/listProject",
+          list: "/openstack/securityGroup/list",
           delete: "/openstack/osInstance/delete",
-          deleteBatch: "/openstack/osInstance/deleteBatch",
-          exportXlsUrl: "/openstack/osInstance/exportXls",
-          importExcelUrl: "/openstack/osInstance/importExcel",
-          powerOnUrl: "/openstack/osInstance/powerOn",
-          shutDownUrl: "/openstack/osInstance/shutDown",
-          rebootByHARDUrl: "/openstack/osInstance/rebootByHARD",
-          rebootBySOFTUrl: "/openstack/osInstance/rebootBySOFT",
-          createSnapshotUrl: "/openstack/osInstance/createSnapshot"
 
         },
         dictOptions:{},
@@ -236,7 +198,7 @@
       }
     },
     created() {
-    this.getSuperFieldList();
+      this.getSuperFieldList();
     },
     computed: {
       importExcelUrl: function(){
@@ -258,38 +220,11 @@
       handleApply(){
         this.$router.push({name: 'os-OsApplyList',params:{}})
       },
-      handleAddUserProject() {
-        if (false ) {
-          this.$message.error("请选择一个部门!")
-        } else {
-          debugger
-          this.$refs.selectUserProjectModal.visible = true;
-        }
-      },
       handleOK(){
-       this.loadData(1)
+        this.loadData(1)
       },
       selectOK(data) {
         alert('ok')
-        // let params = {};
-        // params.depId = this.currentDeptId;
-        // params.userIdList = [];
-        // for (var a = 0; a < data.length; a++) {
-        //   params.userIdList.push(data[a]);
-        // }
-        // console.log(params);
-        // postAction(this.url.edit, params).then((res) => {
-        //   if (res.success) {
-        //     this.$message.success(res.message);
-        //     this.loadData();
-        //   } else {
-        //     this.$message.warning(res.message);
-        //   }
-        // })
-      },
-      handleRouter(record){
-        console.log('handleRoute  '+record)
-        this.$router.push({name: 'project-ProjectSurvey',params:{projectId:record.projectId}})
       },
 
     }
