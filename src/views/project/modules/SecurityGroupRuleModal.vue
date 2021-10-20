f3532a66ce38669c8b2291afc4faf1fa6a805f3c
ypj <ypj@3dee3642-7118-1743-bb2f-632451eefa8e>
2021-10-20 07:15:57 +0000

    規則説明

--- src/views/project/modules/SecurityGroupRuleModal.vue
+++ src/views/project/modules/SecurityGroupRuleModal.vue
@@ -43,12 +43,12 @@
             </a-form-model-item>
 
 
-            <a-form-model-item label="遠程" :labelCol="labelCol" :wrapperCol="wrapperCol">
+            <a-form-model-item label="遠程" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="remoteType">
               <j-dict-select-tag  :triggerChange="true" dictCode="remote_type" @change="changeRemoteType"  v-model="model.remoteType"  placeholder="请选择遠程">
               </j-dict-select-tag>
             </a-form-model-item>
 
-            <a-form-model-item label="CIDR" v-show="showCIDR" :labelCol="labelCol" :wrapperCol="wrapperCol">
+            <a-form-model-item label="CIDR" v-show="showCIDR" :labelCol="labelCol" :wrapperCol="wrapperCol" >
               <a-input placeholder="请输入路由標記" v-model="model.cidr"></a-input>
             </a-form-model-item>
 
@@ -66,9 +66,11 @@
           </a-col>
 
           <a-col :span="10" >
-            <a-form-model-item label="説明"  hasFeedback>
-              <span>安全组是作用于虚拟机网络接口上的一组IP过滤规则。安全组创建完成后，你可以向其中添加规则。</span>
-            </a-form-model-item>
+            <h1>説明</h1>
+            <h4>实例可以关联安全组，组中的规则定义了允许哪些访问到达被关联的实例。安全组由以下三个主要组件组成：</h4>
+            <h4><b>规则：</b> 您可以指定期望的规则模板或者使用定制规则，选项有定制TCP规则、定制UDP规则或定制ICMP规则。</h4>
+            <h4><b>打开端口/端口范围：</b> 您选择的TCP和UDP规则可能会打开一个或一组端口.选择"端口范围"，您需要提供开始和结束端口的范围.对于ICMP规则您需要指定ICMP类型和代码.</h4>
+            <h4><b>远程：</b> 您必须指定允许通过该规则的流量来源。可以通过以下两种方式实现：IP地址块(CIDR)或者来源地址组(安全组)。如果选择一个安全组作为来访源地址，则该安全组中的任何实例都被允许使用该规则访问任一其它实例。</h4>
           </a-col>
 
 
@@ -108,7 +110,7 @@
         },
         wrapperCol: {
           xs: { span: 24 },
-          sm: { span: 17 },
+          sm: { span: 15 },
         },
         confirmLoading: false,
         form: this.$form.createForm(this),
@@ -121,6 +123,9 @@
           ],
           openPortType:[
             { required: true, message: '請选择打开端口!' },
+          ],
+          remoteType:[
+            { required: true, message: '請选择遠程!' },
           ]
         },
         url: {
