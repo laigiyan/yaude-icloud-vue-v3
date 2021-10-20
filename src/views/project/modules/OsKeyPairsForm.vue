d27a7f920bf9a4707233acf9595840997ae84860
fangqing <fangqing@3dee3642-7118-1743-bb2f-632451eefa8e>
2021-10-20 01:23:42 +0000

    秘钥

---
+++ src/views/project/modules/OsKeyPairsForm.vue
@@ -0,0 +1,185 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <a-form-model ref="form" :model="model" :rules="validatorRules" slot="detail">
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="用户名称" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="userId">
+              <a-select v-model="model.userId" @change="getAll" placeholder="请选择用户" >
+                <a-select-option v-for="user in users":value="user.value"  >{{user.text}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" >
+            <a-form-model-item label="項目名稱" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="projectId">
+              <a-select v-model="model.projectId"  placeholder="请选择項目" >
+                <a-select-option v-for="project in projects":value="project.value"  >{{project.text}}</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="秘钥名称" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="keyName">
+              <a-input v-model="model.keyName" placeholder="请输入秘钥名称"  :disabled="editKey"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" v-show="showKey">
+            <a-form-model-item label="指纹" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="fingerprint">
+              <a-textarea v-model="model.fingerprint" placeholder="请输入指纹" :disabled="editKey"></a-textarea>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24" v-show="showKey">
+            <a-form-model-item label="公钥" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="publicKey">
+              <a-textarea v-model="model.publicKey" placeholder="请输入公钥" :disabled="editKey"></a-textarea>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </j-form-container>
+  </a-spin>
+</template>
+
+<script>
+
+  import { httpAction, getAction } from '@/api/manage'
+  import { validateDuplicateValue } from '@/utils/util'
+
+  export default {
+    name: 'OsKeyPairsForm',
+    components: {
+    },
+    props: {
+      //表单禁用
+      disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    data () {
+      return {
+        model:{
+         },
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+        },
+        users:[],
+        projects:[],
+        showKey:false,
+        editKey:false,
+        url: {
+          add: "/openstack/osKeyPairs/add",
+          edit: "/openstack/osKeyPairs/edit",
+          queryById: "/openstack/osKeyPairs/queryById",
+          getUser: "/openstack/osKeyPairs/getUser",
+          getProject: "/openstack/osKeyPairs/getProject",
+          getKeyType: "/openstack/osKeyPairs/getKeyType",
+        }
+      }
+    },
+    computed: {
+      formDisabled(){
+        return this.disabled
+      },
+    },
+    created () {
+       //备份model原始值
+      this.modelDefault = JSON.parse(JSON.stringify(this.model));
+    this.getUser();
+    },
+    methods: {
+      add () {
+        this.edit(this.modelDefault);
+      },
+      edit (record) {
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        if(this.model.id){
+          this.showKey=true;
+          this.editKey=true;
+        }
+      },
+      submitForm () {
+        const that = this;
+        this.users.forEach((r)=>{
+          if(r.value==that.model.userId){
+            that.model.userName = r.text;
+          }
+        })
+        this.projects.forEach((r)=>{
+          if(r.value==that.model.projectId){
+            that.model.projectName = r.text;
+          }
+        })
+        // 触发表单验证
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            httpAction(httpurl,this.model,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+            })
+          }
+
+        })
+      },
+      getAll(){
+        this.getProjects();
+      },
+      getUser(){
+        let method = "post";
+        let httpurl = this.url.getUser;
+        httpAction(httpurl,this.model,method).then((res)=>{
+          if(res.success){
+            const result = res.result
+            debugger
+            result.forEach((r)=>{
+              this.users.push({
+                value:r.userId,
+                text:r.userName,
+              })
+            })
+          }
+        })
+      },
+      getProjects(){
+        let method = "post";
+        let httpurl = this.url.getProject;
+        httpAction(httpurl,this.model,method).then((res)=>{
+          if(res.success){
+            const result = res.result
+            result.forEach((r)=>{
+              this.projects.push({
+                value:r.projectId,
+                text:r.projectName,
+              })
+            })
+          }
+        })
+      },
+
+    }
+  }
+</script>
---
+++ src/views/project/modules/OsKeyPairsModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <j-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    switchFullscreen
+    @ok="handleOk"
+    :okButtonProps="{ class:{'jee-hidden': disableSubmit} }"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <os-key-pairs-form ref="realForm" @ok="submitCallback" :disabled="disableSubmit"></os-key-pairs-form>
+  </j-modal>
+</template>
+
+<script>
+
+  import OsKeyPairsForm from './OsKeyPairsForm'
+  export default {
+    name: 'OsKeyPairsModal',
+    components: {
+      OsKeyPairsForm
+    },
+    data () {
+      return {
+        title:'',
+        width:800,
+        visible: false,
+        disableSubmit: false
+      }
+    },
+    methods: {
+      add () {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.add();
+        })
+      },
+      edit (record) {
+        this.visible=true
+        this.$nextTick(()=>{
+          this.$refs.realForm.edit(record);
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        this.$refs.realForm.submitForm();
+      },
+      submitCallback(){
+        this.$emit('ok');
+        this.visible = false;
+      },
+      handleCancel () {
+        this.close()
+      }
+    }
+  }
+</script>
