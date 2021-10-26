015ba5c65c67a1811155fec78f02d1983e1f2784
fangqing <fangqing@3dee3642-7118-1743-bb2f-632451eefa8e>
2021-10-26 06:30:53 +0000

    簡轉繁

--- src/views/exception/type.js
+++ src/views/exception/type.js
@@ -2,17 +2,17 @@ const types = {
   403: {
     img: 'https://gw.alipayobjects.com/zos/rmsportal/wZcnGqRDyhPOEYFcZDnb.svg',
     title: '403',
-    desc: '抱歉，你无权访问该页面'
+    desc: '抱歉，你無權訪問該頁面'
   },
   404: {
     img: 'https://gw.alipayobjects.com/zos/rmsportal/KpnpchXsobRgLElEozzI.svg',
     title: '404',
-    desc: '抱歉，你访问的页面不存在或无权访问'
+    desc: '抱歉，你訪問的頁面不存在或無權訪問'
   },
   500: {
     img: 'https://gw.alipayobjects.com/zos/rmsportal/RVRUAYdCGeYNBWoKiIwB.svg',
     title: '500',
-    desc: '抱歉，服务器出错了'
+    desc: '抱歉，服務器出錯了'
   }
 }
 
