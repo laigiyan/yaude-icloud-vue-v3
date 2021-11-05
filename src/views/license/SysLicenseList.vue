d2057b0dc088af1ef47f6ae6993349f9be7bdb50
fangqing <fangqing@3dee3642-7118-1743-bb2f-632451eefa8e>
2021-11-05 01:19:42 +0000

    认证

---
+++ src/views/license/SysLicenseList.vue
@@ -0,0 +1,254 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="证书名称">
+              <a-input placeholder="请输入证书名称" v-model="queryParam.subject"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <a-form-item label="密钥别称">
+              <a-input placeholder="请输入密钥别称" v-model="queryParam.privatealias"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :xl="6" :lg="7" :md="8" :sm="24">
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+              <a @click="handleToggleSearch" style="margin-left: 8px">
+                {{ toggleSearchStatus ? '收起' : '展开' }}
+                <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
+              </a>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+      <a-button type="primary" icon="download" @click="handleExportXls('认证证书')">导出</a-button>
+      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+        <a-button type="primary" icon="import">导入</a-button>
+      </a-upload>
+      <!-- 高级查询区域 -->
+      <j-super-query :fieldList="superFieldList" ref="superQueryModal" @handleSuperQuery="handleSuperQuery"></j-super-query>
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <a-menu slot="overlay">
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+      </a-dropdown>
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
+        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+      </div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        :scroll="{x:true}"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+        <template slot="imgSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无图片</span>
+          <img v-else :src="getImgView(text)" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+        </template>
+        <template slot="fileSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">无文件</span>
+          <a-button
+            v-else
+            :ghost="true"
+            type="primary"
+            icon="download"
+            size="small"
+            @click="downloadFile(text)">
+            下载
+          </a-button>
+        </template>
+
+        <span slot="action" slot-scope="text, record">
+          <a @click="generate(record)">生成认证</a>
+
+        <!--  <a @click="handleEdit(record)">编辑</a>-->
+
+          <a-divider type="vertical" />
+          <a-dropdown>
+            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a @click="handleDetail(record)">详情</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                  <a>删除</a>
+                </a-popconfirm>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </span>
+
+      </a-table>
+    </div>
+
+    <sys-license-modal ref="modalForm" @ok="modalFormOk"></sys-license-modal>
+  </a-card>
+</template>
+
+<script>
+
+  import '@/assets/less/TableExpand.less'
+  import { mixinDevice } from '@/utils/mixin'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  import SysLicenseModal from './modules/SysLicenseModal'
+  import { httpAction, getAction } from '@/api/manage'
+
+  export default {
+    name: 'SysLicenseList',
+    mixins:[JeecgListMixin, mixinDevice],
+    components: {
+      SysLicenseModal
+    },
+    data () {
+      return {
+        description: '认证证书管理页面',
+        // 表头
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key:'rowIndex',
+            width:60,
+            align:"center",
+            customRender:function (t,r,index) {
+              return parseInt(index)+1;
+            }
+          },
+          {
+            title:'证书名称',
+            align:"center",
+            dataIndex: 'subject'
+          },
+          {
+            title:'密钥别称',
+            align:"center",
+            dataIndex: 'privateAlias'
+          },
+          {
+            title:'密钥密码',
+            align:"center",
+            dataIndex: 'keyPass'
+          },
+          {
+            title:'证书生效时间',
+            align:"center",
+            dataIndex: 'issuedTime',
+            customRender:function (text) {
+              return !text?"":(text.length>20?text.substr(0,20):text)
+            }
+          },
+          {
+            title:'证书失效时间',
+            align:"center",
+            dataIndex: 'expiryTime',
+            customRender:function (text) {
+              return !text?"":(text.length>20?text.substr(0,20):text)
+            }
+          },
+          {
+            title:'用户类型',
+            align:"center",
+            dataIndex: 'consumerType'
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            fixed:"right",
+            width:147,
+            scopedSlots: { customRender: 'action' }
+          }
+        ],
+        url: {
+          list: "/licensePackage/sysLicense/list",
+          delete: "/licensePackage/sysLicense/delete",
+          deleteBatch: "/licensePackage/sysLicense/deleteBatch",
+          exportXlsUrl: "/licensePackage/sysLicense/exportXls",
+          importExcelUrl: "licensePackage/sysLicense/importExcel",
+          generateLicense: "/license/generateLicense",
+
+        },
+        dictOptions:{},
+        superFieldList:[],
+      }
+    },
+    created() {
+    this.getSuperFieldList();
+    },
+    computed: {
+      importExcelUrl: function(){
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+      },
+    },
+    methods: {
+      initDictConfig(){
+      },
+      generate(record){
+        const that = this;
+        this.model = Object.assign({}, record);
+        let formData= {
+          oldIpAddress: this.model.oldIpAddress
+        };
+        this.model.licenseCheckModel = formData;
+        let method = 'post';
+        let httpurl = this.url.generateLicense;
+        httpAction(httpurl,this.model,method).then((res)=>{
+          if(res.result=="ok"){
+            that.$message.success("認證成功！")
+            that.$emit('ok');
+          }else{
+            that.$message.warning(res.message);
+          }
+        })
+      },
+      getSuperFieldList(){
+        let fieldList=[];
+        fieldList.push({type:'string',value:'subject',text:'证书名称',dictCode:''})
+        fieldList.push({type:'string',value:'privateAlias',text:'密钥别称',dictCode:''})
+        fieldList.push({type:'string',value:'keyPass',text:'密钥密码',dictCode:''})
+        fieldList.push({type:'string',value:'storePass',text:'访问秘钥库的密码',dictCode:''})
+        fieldList.push({type:'string',value:'licensePath',text:'证书生成路径',dictCode:''})
+        fieldList.push({type:'string',value:'privateKeysStorePath',text:'密钥库存储路径',dictCode:''})
+        fieldList.push({type:'date',value:'issuedTime',text:'证书生效时间'})
+        fieldList.push({type:'date',value:'expiryTime',text:'证书失效时间'})
+        fieldList.push({type:'string',value:'consumerType',text:'用户类型',dictCode:''})
+        fieldList.push({type:'int',value:'consumerAmount',text:'用户数量',dictCode:''})
+        fieldList.push({type:'string',value:'description',text:'描述信息',dictCode:''})
+        this.superFieldList = fieldList
+      }
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less';
+</style>
---
+++ src/views/license/modules/SysLicenseForm.vue
@@ -0,0 +1,209 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <a-form-model ref="form" :model="model" :rules="validatorRules" slot="detail">
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="证书名称" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="subject">
+              <a-input v-model="model.subject" placeholder="请输入证书名称"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <!--<a-col :span="24">
+            <a-form-model-item label="密钥别称" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="privateAlias">
+              <a-input v-model="model.privateAlias" placeholder="请输入密钥别称"  ></a-input>
+            </a-form-model-item>
+          </a-col>-->
+          <a-col :span="24">
+            <a-form-model-item label="密钥密码" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="keyPass">
+              <a-input v-model="model.keyPass" placeholder="请输入密钥密码"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="访问秘钥库的密码" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="storePass">
+              <a-input v-model="model.storePass" placeholder="请输入访问秘钥库的密码"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <!--<a-col :span="24">
+            <a-form-model-item label="证书生成路径" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="licensePath">
+              <a-input v-model="model.licensePath" placeholder="请输入证书生成路径"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="密钥库存储路径" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="privateKeysStorePath">
+              <a-input v-model="model.privateKeysStorePath" placeholder="请输入密钥库存储路径"  ></a-input>
+            </a-form-model-item>
+          </a-col>-->
+          <a-col :span="24">
+            <a-form-model-item label="证书生效时间" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="issuedTime">
+              <j-date placeholder="请选择证书生效时间" v-model="model.issuedTime" date-format="YYYY-MM-DD HH:mm:ss" style="width: 100%" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="证书失效时间" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="expiryTime">
+              <j-date placeholder="请选择证书失效时间" v-model="model.expiryTime" date-format="YYYY-MM-DD HH:mm:ss" style="width: 100%" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="用户类型" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="consumerType">
+              <a-input v-model="model.consumerType" placeholder="请输入用户类型"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item label="IP地址" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="ipAddress">
+              <a-input v-model="model.ipAddress" placeholder="用逗號分割輸入的ip"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="MAC地址" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="macAddress">
+              <a-input v-model="model.macAddress" placeholder="用逗號分割MAC地址"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="CPU序列号" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="cpuSerial">
+              <a-input v-model="model.cpuSerial" placeholder="請輸入CPU序列号"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="主板序列号" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="mainBoardSerial">
+              <a-input v-model="model.mainBoardSerial" placeholder="請輸入主板序列号"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+
+          <a-col :span="24">
+            <a-form-model-item label="舊ip地址" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="oldIpAddress">
+              <a-input v-model="model.oldIpAddress" placeholder="请输入舊ip地址"  ></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-model-item label="描述信息" :labelCol="labelCol" :wrapperCol="wrapperCol" prop="description">
+              <a-textarea v-model="model.description" rows="4" placeholder="请输入描述信息" />
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </j-form-container>
+  </a-spin>
+</template>
+
+<script>
+
+  import { httpAction, getAction } from '@/api/manage'
+  import { validateDuplicateValue } from '@/utils/util'
+
+  export default {
+    name: 'SysLicenseForm',
+    components: {
+    },
+    props: {
+      //表单禁用
+      disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    data () {
+      return {
+        model:{
+         },
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+           subject: [
+              { required: true, message: '请输入证书名称!'},
+           ],
+          privateAlias: [
+              { required: true, message: '请输入密钥别称!'},
+           ],
+          keyPass: [
+              { required: true, message: '请输入密钥密码!'},
+           ],
+          storePass: [
+              { required: true, message: '请输入访问秘钥库的密码!'},
+           ],
+          issuedTime: [
+            { required: true, message: '请选择证书生效时间!'},
+          ],
+          expiryTime: [
+            { required: true, message: '请选择证书失效时间!'},
+          ],
+          ipAddress: [
+            { required: true, message: '请输入ip地址!'},
+          ],
+          macAddress: [
+            { required: true, message: '请输入MAC地址!'},
+          ],
+          cpuSerial: [
+            { required: true, message: '请输入CPU序列号!'},
+          ],
+          mainBoardSerial: [
+            { required: true, message: '请输入主板序列号!'},
+          ],
+
+        },
+        url: {
+          add: "/licensePackage/sysLicense/add",
+          edit: "/licensePackage/sysLicense/edit",
+          queryById: "/licensePackage/sysLicense/queryById"
+        }
+      }
+    },
+    computed: {
+      formDisabled(){
+        return this.disabled
+      },
+    },
+    created () {
+       //备份model原始值
+      this.modelDefault = JSON.parse(JSON.stringify(this.model));
+    },
+    methods: {
+      add () {
+        this.edit(this.modelDefault);
+      },
+      edit (record) {
+        this.model = Object.assign({}, record);
+        this.visible = true;
+      },
+      submitForm () {
+        const that = this;
+        // 触发表单验证
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            let a = this.model;
+            debugger
+            httpAction(httpurl,this.model,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+            })
+          }
+
+        })
+      },
+    }
+  }
+</script>
