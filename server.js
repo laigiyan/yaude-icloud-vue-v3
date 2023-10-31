496023c93f2236f9e88ddd13ec5eb74367744ee5
buhuaqiang <79677630+buhuaqiang@users.noreply.github.com>
2023-10-31 13:58:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const { createProxyMiddleware } = require('http-proxy-middleware')
+app.use('/yaude-boot', createProxyMiddleware({
+  // 代理跨域目标接口 例如htt://110.11.11.111/
+  target: 'http://yaude-icloud-java:8080/yaude-boot',
+  //changeOrigin如果设置成false：请求头中host仍然是浏览器发送过来的host；
+  //如果设置成true：发送请求头中host会设置成target。
+  //tips：这个改变并不能在network里直观显示
+  changeOrigin: true,
+}));
 const port = 8080 // 自定义端口号（不要与已存在端口冲突）
 app.use(express.static('dist')) // dist 是项目的打包资源路径，一般为根目录下dist
 app.listen(port, () => console.log(`服务器 ${port} 开启成功!`))
