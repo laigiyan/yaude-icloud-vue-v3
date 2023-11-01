e813a243e51952f9cc4e8b0ffcba199448fb5125
buhuaqiang <79677630+buhuaqiang@users.noreply.github.com>
2023-11-01 15:13:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const  createProxyMiddleware  = require('http-proxy-middleware');
 
 app.use('/yaude-boot', createProxyMiddleware({
   // 代理跨域目标接口 例如htt://110.11.11.111/
-  target: 'http://yaude-icloud-java:8080/',
+  target: 'http://yaude-icloud-java.default.svc.cluster.local/',
   //changeOrigin如果设置成false：请求头中host仍然是浏览器发送过来的host；
   //如果设置成true：发送请求头中host会设置成target。
   //tips：这个改变并不能在network里直观显示
